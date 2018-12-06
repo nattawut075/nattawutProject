ada407a5fe7b62a2891ad9a4f966299cda1257f0
Nattawut075 <b.n.ben123@hotmail.com>
2018-12-06 13:23:46 +0700

    lap
    lap1-2

---
+++ Myproject01/Myproject01/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        lblTotal.Text = Val(txthoure.Text) * Val(txtWork.Text)
+        lblTax.Text = Val(lblTotal.Text) * 3 / 100
+
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCal2.Click
+        Dim hours As Integer
+        Dim work, total, tax As Double
+
+        hours = Val(txthoure.Text)
+        work = Val(txtWork.Text)
+
+        total = hours * work
+        tax = total * 3 / 100
+
+
+        lblTotal.Text = total
+        lblTax.Text = tax
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
---
+++ Myproject01/Myproject01/Myproject2.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Myproject2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtVistra = New System.Windows.Forms.TextBox()
+        Me.txtComputer = New System.Windows.Forms.TextBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lbltotalmis = New System.Windows.Forms.Label()
+        Me.btnCal = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblVismis = New System.Windows.Forms.Label()
+        Me.lblComputer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(311, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 50)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ยอดขาย"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Red
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(34, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(302, 50)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "ยอดขาย Vistra Gluta"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Red
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.CausesValidation = False
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label3.Location = New System.Drawing.Point(34, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(302, 50)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "ยอดขาย Computer Toshiba Extra520x"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(34, 250)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(302, 50)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "ยอดขายรวม"
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.Location = New System.Drawing.Point(34, 481)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(302, 50)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "ค่าคอมมิชชันรวม"
+        '
+        'txtVistra
+        '
+        Me.txtVistra.BackColor = System.Drawing.Color.Red
+        Me.txtVistra.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtVistra.Location = New System.Drawing.Point(431, 108)
+        Me.txtVistra.Multiline = True
+        Me.txtVistra.Name = "txtVistra"
+        Me.txtVistra.Size = New System.Drawing.Size(187, 50)
+        Me.txtVistra.TabIndex = 0
+        '
+        'txtComputer
+        '
+        Me.txtComputer.BackColor = System.Drawing.Color.Red
+        Me.txtComputer.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtComputer.Location = New System.Drawing.Point(431, 172)
+        Me.txtComputer.Multiline = True
+        Me.txtComputer.Name = "txtComputer"
+        Me.txtComputer.Size = New System.Drawing.Size(187, 50)
+        Me.txtComputer.TabIndex = 1
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(431, 250)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(187, 50)
+        Me.lblTotal.TabIndex = 4
+        '
+        'lbltotalmis
+        '
+        Me.lbltotalmis.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lbltotalmis.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbltotalmis.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lbltotalmis.Location = New System.Drawing.Point(431, 481)
+        Me.lbltotalmis.Name = "lbltotalmis"
+        Me.lbltotalmis.Size = New System.Drawing.Size(187, 50)
+        Me.lbltotalmis.TabIndex = 5
+        Me.lbltotalmis.Text = " "
+        '
+        'btnCal
+        '
+        Me.btnCal.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnCal.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnCal.Location = New System.Drawing.Point(390, 559)
+        Me.btnCal.Name = "btnCal"
+        Me.btnCal.Size = New System.Drawing.Size(103, 42)
+        Me.btnCal.TabIndex = 6
+        Me.btnCal.Text = "คำนวณ"
+        Me.btnCal.UseVisualStyleBackColor = False
+        '
+        'btnClose
+        '
+        Me.btnClose.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnClose.Location = New System.Drawing.Point(549, 559)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(101, 42)
+        Me.btnClose.TabIndex = 7
+        Me.btnClose.Text = "ปิด"
+        Me.btnClose.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label6.Location = New System.Drawing.Point(34, 329)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(302, 50)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "ค่าคอมมิช Vistra Gluta"
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label7.Location = New System.Drawing.Point(34, 401)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(302, 50)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "ค่าคอมมิช Computer Toshiba Extra520x"
+        '
+        'lblVismis
+        '
+        Me.lblVismis.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblVismis.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblVismis.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblVismis.Location = New System.Drawing.Point(431, 329)
+        Me.lblVismis.Name = "lblVismis"
+        Me.lblVismis.Size = New System.Drawing.Size(187, 50)
+        Me.lblVismis.TabIndex = 2
+        '
+        'lblComputer
+        '
+        Me.lblComputer.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblComputer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblComputer.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblComputer.Location = New System.Drawing.Point(431, 416)
+        Me.lblComputer.Name = "lblComputer"
+        Me.lblComputer.Size = New System.Drawing.Size(187, 50)
+        Me.lblComputer.TabIndex = 3
+        '
+        'Myproject2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(792, 623)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnCal)
+        Me.Controls.Add(Me.txtComputer)
+        Me.Controls.Add(Me.txtVistra)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbltotalmis)
+        Me.Controls.Add(Me.lblVismis)
+        Me.Controls.Add(Me.lblComputer)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Myproject2"
+        Me.Text = "Myproject2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtVistra As TextBox
+    Friend WithEvents txtComputer As TextBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lbltotalmis As Label
+    Friend WithEvents btnCal As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblVismis As Label
+    Friend WithEvents lblComputer As Label
+End Class
